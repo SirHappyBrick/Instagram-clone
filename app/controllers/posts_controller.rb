adcea014ab89d0d14a88cabb0495169c28945e81
Oskar <oskar.madej.wroc@gmail.com>
2021-10-26 05:23:08 +0200

    +crud for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,36 @@ class PostsController < ApplicationController
   def new 
     @post = current_user.posts.build
   end
+
+  def create 
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post 
+    else 
+      render 'new'
+    end 
+  end 
+
+  def show 
+    @post = Post.find(params[:id])
+  end 
+
+  def edit 
+    @post = Post.find(params[:id])
+  end 
+
+  def update 
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post 
+    else 
+      render 'edit'
+    end
+  end
+
+  private 
+
+  def post_params 
+    params.require(:post).permit(:title, :description)
+  end 
 end
