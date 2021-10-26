487f71dc73fc89872bc070944742af40d989fc5b
Oskar <oskar.madej.wroc@gmail.com>
2021-10-26 04:36:57 +0200

    + User model and asociacions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'posts#index'
 end
---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2021_10_26_023409) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
